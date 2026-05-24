e77fd15017f6966423acfcb399e3f9b8a6c6547f
DevSamSilva <fcosamueldsn@gmail.com>
2026-05-24 16:42:29 -0300

    acrescentando cometarios aos posts

--- src/main/java/com/samuelsilva/workshopmongo/config/Instantiation.java
+++ src/main/java/com/samuelsilva/workshopmongo/config/Instantiation.java
@@ -3,6 +3,7 @@ package com.samuelsilva.workshopmongo.config;
 import com.samuelsilva.workshopmongo.domain.Post;
 import com.samuelsilva.workshopmongo.domain.User;
 import com.samuelsilva.workshopmongo.dto.AuthorDTO;
+import com.samuelsilva.workshopmongo.dto.CommentDTO;
 import com.samuelsilva.workshopmongo.repository.PostRepository;
 import com.samuelsilva.workshopmongo.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -40,6 +41,14 @@ public class Instantiation implements CommandLineRunner {
 
         Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu Viagem!", "Vou viajar para São paulo. Abraços!", new AuthorDTO(maria));
         Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia!", "Acordei feliz hoje!!", new AuthorDTO(maria));
+
+        CommentDTO c1 = new CommentDTO("Boa viagem, mana!", sdf.parse("21/03/2018"), new AuthorDTO(alex));
+        CommentDTO c2 = new CommentDTO("Aproveite!", sdf.parse("22/03/2018"), new AuthorDTO(bob));
+        CommentDTO c3 = new CommentDTO("Tenha um otimo dia!", sdf.parse("23/03/2018"), new AuthorDTO(alex));
+
+        post1.getComments().addAll(Arrays.asList(c1,c2));
+        post2.getComments().addAll(Arrays.asList(c3));
+
 
         postRepository.saveAll(Arrays.asList(post1, post2));
 
