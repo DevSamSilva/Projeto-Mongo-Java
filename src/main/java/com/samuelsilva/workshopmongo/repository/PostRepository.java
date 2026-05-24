02abf8395d4cb1c314da63e7ac2545965c3add6d
DevSamSilva <fcosamueldsn@gmail.com>
2026-05-24 17:27:44 -0300

    consulta simples com @query

--- src/main/java/com/samuelsilva/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/samuelsilva/workshopmongo/repository/PostRepository.java
@@ -2,13 +2,20 @@ package com.samuelsilva.workshopmongo.repository;
 
 import com.samuelsilva.workshopmongo.domain.Post;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post,String > {
+
+    @Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+    List<Post> searchTitle(String text);
+
     List<Post> findByTitleContainingIgnoreCase(String text);
+
+
 }
 
 
